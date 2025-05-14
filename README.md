39b5592c97984bd12b3496982be9df46492dc32a
roger <rogerlim799@gmail.com>
2025-05-14 11:39:39 +0800

    delete acc & confirm password

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# **RoamWise**  
-Level of Achievement: Artemis  
-RoamWise is a comprehensive travel planning web application that simplifies trip organization by combining AI-powered itinerary generation, budget tracking, and collaborative tools in one platform. It caters to both solo and group travelers with personalized recommendations and a community forum for sharing and exploring travel plans. Designed for ease and efficiency, RoamWise helps users plansmarter and travel with confidence.
+npm install
+npm install firebase
+npm install --save react-toastify
+npm run dev
--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { auth, db } from "./firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, deleteDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 
 type UserDetails = {
@@ -48,6 +48,21 @@ function Profile() {
     }
   };
 
+  const handleDelete = async () => {
+    try {
+      const user = auth.currentUser;
+      if (user) {
+        await deleteDoc(doc(db, "Users", user.uid));
+        await user.delete();
+        navigate("/login");
+      } else {
+        console.error("No user is currently logged in.");
+      }
+    } catch (error) {
+      console.error("Error delete account:", error.message);
+    }
+  };
+
   if (!authChecked) {
     return <p>Loading...</p>;
   }
@@ -71,102 +86,18 @@ function Profile() {
         <p>First Name: {userDetails.firstName}</p>
         {/* <p>Last Name: {userDetails.lastName}</p> */}
       </div>
-      <button className="btn btn-primary" onClick={handleLogout}>
-        Logout
-      </button>
+      <div className="mb-2">
+        <button className="btn btn-primary" onClick={handleLogout}>
+          Logout
+        </button>
+      </div>
+      <div>
+        <button className="btn btn-danger" onClick={handleDelete}>
+          Delete Account
+        </button>
+      </div>
     </div>
   );
 }
 
 export default Profile;
-
-// import React, { useEffect, useState } from "react";
-// import { auth, db } from "./firebase";
-// import { doc, getDoc } from "firebase/firestore";
-// import { useNavigate } from "react-router-dom";
-
-// function Profile() {
-// const navigate = useNavigate();
-//   const [userDetails, setUserDetails] = useState(null);
-// //   const [authLoading, setAuthLoading] = useState(true);
-// //   const fetchUserData = async () => {
-// //     auth.onAuthStateChanged(async (user) => {
-// //       console.log(user);
-// //       const docRef = doc(db, "Users", user.uid);
-// //       const docSnap = await getDoc(docRef);
-// //       if (docSnap.exists()) {
-// //         setUserDetails(docSnap.data());
-// //         console.log(docSnap.data());
-// //       } else {
-// //         console.log("User is not logged in");
-// //       }
-// //     });
-// //   };
-
-// const fetchUserData = () => {
-//     auth.onAuthStateChanged(async (user) => {
-//       if (!user) {
-//         console.log("No user is logged in.");
-//         setUserDetails(null);
-//         return;
-//       }
-
-//       try {
-//         const docRef = doc(db, "Users", user.uid);
-//         const docSnap = await getDoc(docRef);
-//         if (docSnap.exists()) {
-//           setUserDetails(docSnap.data());
-//           console.log(docSnap.data());
-//         } else {
-//           console.log("User document does not exist.");
-//         }
-//       } catch (err) {
-//         console.error("Error fetching user data:", err.message);
-//       }
-//     });
-//   };
-
-//  useEffect(() => {
-//     fetchUserData();
-//   }, []);
-
-//   async function handleLogout() {
-//     try {
-//       await auth.signOut();
-//     //   window.location.href = "/login";
-//       navigate("/login");
-//       console.log("User logged out successfully!");
-//     } catch (error) {
-//       console.error("Error logging out:", error.message);
-//     }
-//   }
-
-//   return (
-//     <div>
-//       {userDetails ? (
-//         <>
-//           <div style={{ display: "flex", justifyContent: "center" }}>
-//             <img
-//               src={userDetails.photo}
-//               width={"40%"}
-//               style={{ borderRadius: "50%" }}
-//             />
-//           </div>
-//           <h3>Welcome to RoamWise {userDetails.firstName} </h3>
-//           <div>
-//             <p>Email: {userDetails.email}</p>
-//             <p>First Name: {userDetails.firstName}</p>
-//             <p>Last Name: {userDetails.lastName}</p>
-//           </div>
-//           <button className="btn btn-primary" onClick={handleLogout}>
-//             Logout
-//           </button>
-//         </>
-//       ) : (
-//         <p>Loading...</p>
-//       )}
-//     </div>
-//   );
-
-// }
-// export default Profile;
