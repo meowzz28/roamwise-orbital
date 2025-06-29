1c0e92c60f82b2a5949aff0739b584e723908108
YSTey0808 <ystey0808@gmail.com>
2025-06-29 12:23:26 +0800

    MS2 Unit Testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,25 +1,26 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import { describe, it, expect, vi } from "vitest";
-import { BrowserRouter } from "react-router-dom"; // ✅ Needed for useNavigate
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { act } from "react";
 
-// Mock Firebase auth
 vi.mock("./components/firebase", () => ({
   auth: {
     onAuthStateChanged: (callback: any) => {
-      callback({ uid: "123" }); // simulate logged-in user
-      return () => {}; // mock unsubscribe
+      callback({ uid: "123" });
+      return () => {};
     },
   },
 }));
-
 describe("App Component", () => {
-  it("renders RoamWise after loading", async () => {
-    render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
+  it("Renders RoamWise after loading", async () => {
+    await act(async () => {
+      render(
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      );
+    });
 
     await waitFor(() => {
       expect(screen.getAllByText("RoamWise").length).toBeGreaterThan(0);
